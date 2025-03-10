e0affbae3f531ed1f77124245e559b277c969108
Guna Palani <gupalani@freshworks.com>
2025-03-10 14:23:55 +0530

    delete 2.0

--- playwright2.0/Ecom-POM/homepage.js
+++
@@ -1,9 +0,0 @@
-class HomePage{
-    constructor(page){
-       this.page = page;
-    }
-    async navigatetoWeb(){
-       await this.page.goto('/')
-    }
-   }
-   module.exports=HomePage
--- playwright2.0/tests/homepage.test.js
+++
@@ -1,6 +0,0 @@
-const {test,expect}= require ("@playwright/test")
-
-test("open the homepage and check the title", async function({page}){
-    await page.goto("/")
-    await expect(page).toHaveTitle(/Home/)
-})
