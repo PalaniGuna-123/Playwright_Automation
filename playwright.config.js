66ae53d86c9fbf4f68d8b8d6c9180bf6c14f4650
Guna Palani <gupalani@freshworks.com>
2025-03-15 16:21:54 +0530

    learn dropdown

--- playwright.config.js
+++ playwright.config.js
@@ -19,7 +19,8 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
+  retries:2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -28,12 +29,13 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://magento.softwaretestingboard.com/',
-    headless: false,
+    // headless: false,
+    headless: true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
-    screenshot:"on",
-    video: "on"
+    trace:"off",
+    screenshot:"off",
+    video: "off"
   },
 
   /* Configure projects for major browsers */
---
+++ tests/dropdown.spec.js
@@ -0,0 +1,37 @@
+import {test,expect} from '@playwright/test'
+test ("My dropdown test", async ({page})=>{
+    await page.goto("https://www.globalsqa.com/demo-site/select-dropdown-menu/")
+    await page.locator('//*[@id="post-2646"]/div[2]/div/div/div/p/select').selectOption({index:4})
+    await page.waitForTimeout(3000)
+
+    const value=await page.locator('//*[@id="post-2646"]/div[2]/div/div/div/p/select').textContent()
+    console.log("All dropdown values "+ value);
+
+    await expect(value.includes('BritishVirgin Islands')).toBeTruthy()
+
+    let country=await page.$('//*[@id="post-2646"]/div[2]/div/div/div/p/select')
+    
+    let array=await country.$$("option")
+    
+    let status=false
+    for(let i=0;i<array.length;i++){
+        let element=array[i]
+        let values =await element.textContent()
+        console.log("value from dropdown using for loop  "+ values);
+
+        if(values.includes("Cameroon")){
+            status=true;
+            break;
+        }
+    }
+    
+   await expect(status).toBeTruthy()
+
+    
+})
+
+test.skipgit ("multi select dropdown", async ({page})=>{
+    await page.goto("https://demoqa.com/select-menu")
+
+    await page.locator("//div[contains(@class,'css-1pahdxg-control')]//div[contains(@class,'css-1hwfws3')]").selectOption('Green','Blue')
+})
